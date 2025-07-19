8b456758499f969e84a3aea594b3f0d68bea9ecf
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-07-19 21:18:31 +0600

    customize valodation and add password

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,21 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelper/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import { StatusCodes } from "http-status-codes";
 
 const createUser = async (payload: Partial<IUser>)=>{
-    const { name, email } = payload
-    const user = await User.create({name, email})
+    const {  email, ...rest } = payload
+    const isuserExixst = await User.findOne({email})
+
+    if(isuserExixst){
+        throw new AppError(StatusCodes.BAD_REQUEST, "User Already Exist")
+    }
+    const authProvider : IAuthProvider = { provider: "credentials", providerId: email as string}
+
+    const user = await User.create({ 
+        email, 
+        auths: [authProvider],
+        ...rest})
 
     return user
 };
