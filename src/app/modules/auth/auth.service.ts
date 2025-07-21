3d60b4c5171e6fb3fbc831f7e1ec8deb1f828ddc
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-07-21 09:52:53 +0600

    Implemented JWT login setup with token generation

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,7 +3,8 @@ import { StatusCodes } from "http-status-codes";
 import AppError from "../../errorHelper/AppError";
 import { IUser } from "../user/user.interface"
 import { User } from "../user/user.model";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
+import { genaretToken } from "../../utils/jwt";
 
 const creadentialLogin = async (paylode: Partial<IUser>)=>{
     if (!paylode) {
@@ -23,8 +24,14 @@ const creadentialLogin = async (paylode: Partial<IUser>)=>{
         throw new AppError(StatusCodes.BAD_REQUEST , " Incorrect Password")
     }
 
+    const jwtPayloade ={
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+    const accessToken = genaretToken(jwtPayloade, process.env.JWT_ACCESS_SECRET as string, process.env.JWT_ACCESS_EXPIRES as string)
     return{
-        email: isUserExist.email
+        accessToken
     }
 }
 export const authService ={
