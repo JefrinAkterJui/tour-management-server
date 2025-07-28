bcd6ffccf1b49c0336f39fc1f4c1a02132c472ac
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-07-28 23:50:08 +0600

    toure modules done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.routes";
 import { AuthRoute } from "../modules/auth/auth.routes";
 import { DivissionsRoutes } from "../modules/divission/division.routes";
+import { TourRoutes } from "../modules/tour/tour.routes";
 
 export const router= Router()
 
@@ -17,6 +18,10 @@ export const moduleRoutes = [
     {
         path:'/divission',
         route: DivissionsRoutes
+    },
+    {
+        path: "/tour",
+        route: TourRoutes
     }
 ];
 
